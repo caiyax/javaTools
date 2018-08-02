bf3490aeff29e4e1ca6d69e0f07d38078abf0645
Yanan Cai <caiyananyn@rd.netease.com>
2018-08-02 23:44:10 +0800

    LRUMap

---
+++ src/main/java/com/bupt/LRUCache/LRUMap.java
@@ -0,0 +1,47 @@
+package com.bupt.LRUCache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+/*
+* LinkedHashMap提供了按照访问顺序存储的功能，
+* 需要做的就是在元素大于容量的时候删除最久未用的元素即可
+* */
+public class LRUMap<K,V> extends LinkedHashMap<K,V> implements Map<K,V> {
+    private int maxSize; //缓存的容量大小
+
+    public LRUMap(int maxSize){
+        //accessOrder为true是，访问顺序会变成按照访问顺序存储
+        super((int) Math.ceil((1f*maxSize)/0.75f)+16,0.75f,true);
+        this.maxSize=maxSize;
+    }
+
+    @Override
+    protected synchronized boolean removeEldestEntry(Map.Entry<K,V> eldest){
+        return size()>maxSize;
+    }
+
+    @Override
+    public synchronized int size(){
+        return super.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+    @Override
+    public synchronized V get(Object key){
+        return super.get(key);
+    }
+
+    @Override
+    public V remove(Object key) {
+        return super.remove(key);
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+    }
+}
